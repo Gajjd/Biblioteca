7d34e4190d0d8337de5ed0be8249a084665417ce
Gajjd <gabrielandrighetto0@gmail.com>
2026-05-07 10:06:05 -0400

    Adicionando cadastro Livro

---
+++ Biblioteca/Models/Livro.cs
@@ -0,0 +1,16 @@
+
+namespace Biblioteca.Models;
+
+public class Livro
+{
+    public int Id {get; set; }
+    public string? Slug { get; set; }
+    public string? Titulo {get; set; }
+    public string? Imagem { get; set; }
+    public string? Autor {get; set; }
+    public int QtdPaginas {get; set; }
+    public int DataPublicacao {get; set; }
+    public string? Genero {get; set; }
+
+    public string? Resumo {get; set; }
+}
